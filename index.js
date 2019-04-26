d8744f94c56667097753efff5fa967dc0fd5a5f8
Kelliann Lafferty <kell10192@gmail.com>
2019-04-26 04:46:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = greetinglear + specialGuest + "!";
+var greetSpecialGuest = greeting + specialGuest + "!";
 
 var topic = "space";
 
